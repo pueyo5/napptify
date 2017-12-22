0bae0538fa10e260e1978ba1f17fbeef64088198
Albert Pueyo <albert@mhunters.com>
2017-12-22 20:10:20 +0100

    Add router

---
+++ src/components/modal_switch/ModalSwitch.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Home } from "../../scenes/home/Home";
+import { Switch, Route } from 'react-router-dom'
+
+export class ModalSwitch extends Component {
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={Home}/>
+      </Switch>
+    )
+  }
+}
