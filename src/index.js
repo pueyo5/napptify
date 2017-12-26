efae052cdf0b8742f665964d73f4aaef9abfbecd
Albert Pueyo <albert@mhunters.com>
2017-12-26 16:51:11 +0100

    Update project scripts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Home } from './scenes/home/Home'
 import './index.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 render(
   <div>
